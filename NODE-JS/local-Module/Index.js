ba088b57078aee4aee86aa21c4c8990ea5ed1c9f
satyaki95 <www.satyaki95@gmail.com>
2024-08-05 15:56:17 +0530

    import & Export

---
+++ NODE-JS/local-Module/Index.js
@@ -0,0 +1,15 @@
+// const s1 = require('./module');
+
+// console.log(s1.name());
+// console.log(s1.age());
+// console.log(s1.cgpa);
+
+
+// const {getName,Collage} = require('./module');
+const {Collage} = require('./module');
+
+// console.log(getName());
+console.log(Collage());
+
+
+
---
+++ NODE-JS/local-Module/module.js
@@ -0,0 +1,22 @@
+const getName = () => {
+    return "Satyaki";
+}
+const getAge = () => {
+    return "30";
+}
+
+const cgpa = 5.2;
+
+// exports.name = getName;
+// exports.age = getAge;
+// exports.cgpa = cgpa;
+
+// module.exports = {
+//     getName,
+//     getAge,
+//     cgpa
+// }
+
+exports.Collage = () => {
+    return "RCCIIT";
+}
