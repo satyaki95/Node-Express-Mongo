46d821e4cb2f7c76daccaabb9fa3983d4388ef09
satyaki95 <www.satyaki95@gmail.com>
2024-08-07 01:40:29 +0530

    NPM package install

---
+++ NODE-JS/External-Module/index.js
@@ -0,0 +1,5 @@
+const getRandomFruitsName = require('random-fruits-name');
+console.log(getRandomFruitsName());
+
+const getMoviesName = require('movies-names');
+console.log(getMoviesName.random());
