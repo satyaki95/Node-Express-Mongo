2610bcce03e24e2822f80b2555c6e9cf94f5f990
satyaki95 <www.satyaki95@gmail.com>
2024-08-07 00:54:01 +0530

    Server

---
+++ NODE-JS/http-Module/index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const port = 3000;
+const localHost = '127.0.0.1';
+
+// http.createServer((req, res) => {
+//     res.end("Hello. I am Sarver");
+// }).listen(3000);
+
+const myServer = http.createServer((req, res) => {
+    res.end("Hello. I am Sarver");
+});
+
+myServer.listen(port, localHost, () => {
+    console.log(`Server is running at http://${localHost}:${port}`);
+})
