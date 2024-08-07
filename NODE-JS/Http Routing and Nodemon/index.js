a1e512901be8566b270f9faac9c3a5b12cf8b606
satyaki95 <www.satyaki95@gmail.com>
2024-08-07 16:20:37 +0530

    Routing and Nodemon

---
+++ NODE-JS/Http Routing and Nodemon/index.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const fs = require("fs");
+const PORT = 3000;
+const hostName = "127.0.0.1";
+
+const server = http.createServer((req, res) => {
+  const handleReadFile = (statusCode, fileLocation) => {
+    fs.readFile(fileLocation, (err, data) => {
+      res.writeHead(statusCode, { "content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  };
+
+  if (req.url === "/") {
+    handleReadFile(200, "./views/index.html");
+  } else if (req.url === "/contact") {
+    handleReadFile(200, "./views/contact.html");
+  } else if (req.url === "/about") {
+    handleReadFile(200, "./views/about.html");
+  } else {
+    handleReadFile(404, "./views/error.html");
+  }
+});
+
+server.listen(PORT, hostName, () => {
+  console.log(`Server running on http://${hostName}:${PORT}/`);
+});
