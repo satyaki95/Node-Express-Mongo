f1ec92dfdeb5da5742efc3363f764928d2e86871
satyaki95 <www.satyaki95@gmail.com>
2024-08-09 03:26:25 +0530

    Http Methods

--- Express-JS/http Methods/app.js
+++ Express-JS/http Methods/app.js
@@ -1,4 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+  res.end();
+});
+app.get("/about", (req, res) => {
+  res.send("About");
+  res.end();
+});
+app.post("/", (req, res) => {
+  res.send("Post");
+  res.end();
+});
+app.put("/", (req, res) => {
+  res.send("Put");
+  res.end();
+});
+app.delete("/", (req, res) => {
+  res.send("delete");
+  res.end();
+});
 
 module.exports = app;
--- Express-JS/http Methods/index.js
+++ Express-JS/http Methods/index.js
@@ -2,5 +2,5 @@ const app = require("./app");
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
